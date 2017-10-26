4de41a719f8467f952bddf7bf038d06d135ed29e
SalAlba <saljan93@live.com>
2017-10-26 12:20:09 +0200

    convertinf from string to number and more
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Unary_plus

---
+++ unary-plus.js
@@ -0,0 +1,16 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Unary_plus
+   
+    let x = '33';
+		let y = 11;
+		let z = '77';
+
+		console.log(x + +y);
+		console.log(y + +x);
+		
+		console.log(+z + +x);
+		console.log(+x + +z);
+
+		console.log(x + y);
+		console.log(y + x);
+		console.log(y + y);
+		console.log(x + x);
